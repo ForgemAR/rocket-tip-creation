ba69010e4ddbd7b82a4d6c79097ed5756af4d5a4
Diez B. Roggisch <deets@web.de>
2014-08-05 21:06:15 +0200

    Added solid-based scaffold

--- cone-length.py
+++ cone-length.py
@@ -1,5 +1,7 @@
 # this script will solve for the length of the cone so
 # that we know the length of it for a given diameter.
+import solid
+
 from sympy.solvers import solve
 from sympy import Symbol
 
@@ -29,6 +31,10 @@ alu_tip_diameter = 41.0
 slice_steps = 10.0
 # render quality in scad
 fn = 100
+# size of the center notch to align the parts on
+center_notch = 10.0
+# diameter of the milling tool
+tool_radius = 3.0
 
 bottom_cone_length = compute_cone_length(diameter)
 assert 380.0 < bottom_cone_length < 450.0
@@ -48,22 +54,44 @@ def frange(start, end, step):
         yield start
         start += step
 
-cylinders = []
-
-for slice_num, (lower, upper) in enumerate(slice_parameters()):
+def produce_slice(lower, upper, outer_offset=0.0):
+    res = []
     step = (upper - lower) / slice_steps
     for i, f in enumerate(frange(lower, upper, step)):
         r1 = cone_at(f)
         r2 = cone_at(f + step)
-        offset = i * step + mdf_strength * slice_num
-        cylinders.append("translate([0, 0, %(offset)f]) cylinder(h=%(step)f, r1=%(r1)f, r2=%(r2)f, $fn=%(fn)i);" % dict(
+        offset = i * step + outer_offset
+        res.append("translate([0, 0, %(offset)f]) cylinder(h=%(step)f, r1=%(r1)f, r2=%(r2)f, $fn=%(fn)i);" % dict(
             step=step,
             r1=r1,
             r2=r2,
             offset=offset,
             fn=fn
             ))
+    return res
 
-print "translate([0, 0, %(cone_length)f]) rotate([0, 180, 0]){" % dict(cone_length=cone_length)
-print "\n".join(cylinders)
-print "}"
+
+def scaffold(color=[0, 0, 1, 1]):
+    h = solid.translate([diameter / 2.0, 0, cone_length / 2.0])(
+        solid.scale([diameter, diameter, cone_length])(
+            solid.cube(center=True)
+            )
+    ) + solid.translate([0, 0, cone_length / 2.0])(
+        solid.scale([center_notch + tool_radius * 2,
+                     center_notch, cone_length])(
+            solid.cube(center=True)
+            )
+    )
+    return solid.scad_render(solid.color(color)(h))
+
+
+def full_cone():
+    cylinders = []
+    for slice_num, (lower, upper) in enumerate(slice_parameters()):
+        cylinders.extend(produce_slice(lower, upper, mdf_strength * slice_num))
+
+    print "translate([0, 0, %(cone_length)f]) rotate([0, 180, 0]){" % dict(cone_length=cone_length)
+    print "\n".join(cylinders)
+    print "}"
+
+print scaffold()
